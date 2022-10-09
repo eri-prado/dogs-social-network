5d220f8039d3cc550cb30cc7880ff1539dc1b100
Erinaldo Prado <eri.prado@hotmail.com>
2022-10-09 12:01:24 -0300

    general changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login/Login'
 
 function App() {
 
   return (
     <div className="App">
-        ola mundo
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
